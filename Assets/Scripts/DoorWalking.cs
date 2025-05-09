7fd42e7eaf784e34a16453847b4f0c28a9b2826b
Masterpaul562 <9652749@graniteschools.org>
2025-05-09 17:04:50 -0600

    yay

---
+++ Assets/Scripts/DoorWalking.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorWalking : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    private Transform exit;
+    private float y;
+
+    private void Start()
+    {
+        exit = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y= Input.GetAxisRaw("Vertical");
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(y ==1 && other.gameObject == player)
+        {
+            player.transform.position = exit.position;
+        }
+    }
+}
